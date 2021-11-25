504721e173fbe18002f241f01f32758096ed549c
Antosser <anton.moonalien@gmail.com>
2021-11-25 21:34:34 +0100

    Updated parameters

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int filecount = 10;
 char characters[16] = { 48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102 };
 long long done = 0;
 long long max = 1;
+bool hex = false;
 
 void printPercent();
 void recursivCreate(int depth, int goal, std::string prefix);
@@ -17,7 +18,7 @@ int main(int argc, char** argv) {
 	int depth = 6;
 	bool defdepth = false;
 	bool deffilesize = false;
-	std::string name;
+	std::string name = "recursive";
 	bool defname = false;
 
 	for (int i = 1; i < argc; i++) {
@@ -50,6 +51,45 @@ int main(int argc, char** argv) {
 			filecount = atoi(argv[i]);
 			continue;
 		}
+		if (cparameter == "-depth") {
+			i++;
+			if (i == argc) {
+				std::cout << "Expected an argument\n";
+				exit(1);
+			}
+			depth = atoi(argv[i]);
+			defdepth = true;
+			continue;
+		}
+		if (cparameter == "-size") {
+			i++;
+			if (i == argc) {
+				std::cout << "Expected an argument\n";
+				exit(1);
+			}
+			filesize = atoi(argv[i]);
+			deffilesize = true;
+			continue;
+		}
+		if (cparameter == "-name") {
+			i++;
+			if (i == argc) {
+				std::cout << "Expected an argument\n";
+				exit(1);
+			}
+			name = argv[i];
+			defname = true;
+			continue;
+		}
+		if (cparameter == "-hex") {
+			i++;
+			if (i == argc) {
+				std::cout << "Expected an argument\n";
+				exit(1);
+			}
+			hex = true;
+			continue;
+		}
 	}
 
 	//std::cout << depth << ", " << filesize << ", " << filecount << std::endl;
@@ -58,8 +98,8 @@ int main(int argc, char** argv) {
 	}
 	std::thread(printPercent).detach();
 
-	std::filesystem::remove_all("Recursive!\\");
-	recursivCreate(1, depth, "Recursive!\\");
+	std::filesystem::remove_all((name + "\\").c_str());
+	recursivCreate(1, depth, (name + "\\").c_str());
 	std::cout << "100%\n";
 }
 
@@ -68,9 +108,14 @@ void recursivCreate(int depth, int goal, std::string prefix) {
 	if (depth == goal) {
 		for (int i = 0; i < filecount; i++) {
 			std::string path = prefix;
-			for (int j = 0; j < 4; j++) {
-				path += characters[((char*)&i)[3 - j] >> 4 & 15];
-				path += characters[((char*)&i)[3 - j] & 15];
+			if (hex) {
+				for (int j = 0; j < 4; j++) {
+					path += characters[((char*)&i)[3 - j] >> 4 & 15];
+					path += characters[((char*)&i)[3 - j] & 15];
+				}
+			}
+			else {
+				path += i;
 			}
 			std::ofstream(path).close();
 			std::filesystem::resize_file(std::filesystem::path(path), filesize);
@@ -80,9 +125,14 @@ void recursivCreate(int depth, int goal, std::string prefix) {
 	}
 	for (int i = 0; i < filecount; i++) {
 		std::string path = prefix;
-		for (int j = 0; j < 4; j++) {
-			path += characters[((char*)&i)[3 - j] >> 4 & 15];
-			path += characters[((char*)&i)[3 - j] & 15];
+		if (hex) {
+			for (int j = 0; j < 4; j++) {
+				path += characters[((char*)&i)[3 - j] >> 4 & 15];
+				path += characters[((char*)&i)[3 - j] & 15];
+			}
+		}
+		else {
+			path += i;
 		}
 		path += '\\';
 		recursivCreate(depth + 1, goal, path);
